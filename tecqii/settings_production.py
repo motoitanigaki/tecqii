dadf587064a9567ec4e28219e30f346f67792aab
motoitanigaki <orehajikoranai@yahoo.co.jp>
2018-05-27 11:55:44 +0800

    implementing

---
+++ tecqii/settings_production.py
@@ -0,0 +1,12 @@
+from .settings_base import *
+
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'projectdata',
+       'USER': 'projectdatauser',
+       'PASSWORD': 'password',
+       'HOST': 'localhost',
+       'PORT': '',
+   }
+}
